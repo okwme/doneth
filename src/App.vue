c179f9d4cc5d863ad15e0f723c7e459063086308
Trevor Clarke <besnoid@gmail.com>
2017-10-21 16:16:46 -0700

    initial toolbar setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
+    <toolbar></toolbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Toolbar from '@/components/Toolbar'
 import { mapActions } from 'vuex'
 export default {
   name: 'app',
@@ -15,17 +17,31 @@ export default {
   },
   mounted () {
     this.checkAccount()
+  },
+  components: {
+    Toolbar
   }
 }
 </script>
 
 <style>
+html {
+  background: #ffffff;
+}
+body {
+  background: #efefef;
+  display: flex;
+  height: 100vh;
+  padding: 0px;
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
   margin-top: 60px;
 }
 </style>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>
-    <div>
-      sdkfjhlasjdfhjs dlf
+  <div class="toolbar">
+    <div class="logo">
+      <h1>Don.eth</h1>
     </div>
-    <div>
-      sdfsdfa
-    </div>
-    <div @click="clicked">click me</div>
-    <div v-for="option in options" @click="selectCurrency(option)"></div>
-    {{ currencySentence }}
+
+    <ul class="menu">
+      <li @click="navigate('deploy')">Create</li>
+      <li class="currency">
+        <div class="active-currency">{{activeCurrency}} <img src="static/downArrow.svg"/></div>
+        <div class="dropdown">
+          <div class="dropdown-item" v-for="option in options" @click="selectCurrency(option)">Eth/{{option}}</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -24,7 +28,7 @@ export default {
     }
   },
   mounted () {
-    conosole.log(this.options)
+    console.log(this.options)
   },
   methods: {
     click () {
@@ -35,12 +39,88 @@ export default {
     }
   },
   computed: {
-    currencySentence () {
-      return this.currency + ' is my currency'
+    activeCurrency () {
+      return `Eth/${this.currency}`
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+  .toolbar {
+    background: white;
+    box-shadow: 0 1px 10px -3px rgba(0,0,0,0.15);
+    color: #4a4a4a;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0px;
+    position: fixed;
+    top:0;
+    width: 100%;
+    z-index: 99;
+  }
+
+  .logo h1 {
+    color: #548c4c;
+    margin: 10px 20px;
+  }
+
+  .menu {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    margin: 0;
+    min-width: 300px;
+    padding: 10px 10px;
+  }
+
+  .menu li {
+    color: #323232;
+    flex: 1;
+    font-size: 14pt;
+    font-weight: 500;
+    /*margin: auto 20px;*/
+    padding: 10px 10px;
+  }
+  .menu li:hover {
+    color: #548c4c;
+    cursor: pointer;
+  }
+  .menu li.currency {
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 3px;
+    font-size: 12pt;
+    position: relative;
+    text-transform: uppercase;
+  }
+  .dropdown {
+    background: white;
+    box-shadow: 0 1px 10px -3px rgba(0,0,0,0.15);
+    display: none;
+    position: absolute;
+    top: 45px;
+    left: 0px;
+    width: 100%;
+  }
+  .menu li.currency:hover .dropdown {
+    display: block;
+  }
+  .dropdown-item {
+    color: #323232;
+    cursor: pointer;
+    padding: 5px 10px;
+  }
+  .dropdown-item:hover {
+    color: #548c4c;
+  }
+
+  .active-currency {
+    position: relative;
+  }
+  .active-currency img {
+    position: absolute;
+    right: 5px;
+    top: 38%;
+  }
 </style>
