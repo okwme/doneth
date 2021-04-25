a4769972ea53688138274b2e3f8648ed78273e2d
billy rennekamp <billy.rennekamp@gmail.com>
2021-04-25 17:16:57 +0200

    same?

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,31 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    parser: 'babel-eslint'
   },
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    // 'standard'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue'
   ],
   // add your custom rules here
-  'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
+  rules: {
     // allow async-await
-    'generator-star-spacing': 0,
+    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  },
-  'globals': {
-    'web3' : true,
-    'ga' : true,
-    'BrowserSolc' : true,
-    '_web3': true
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "template-curly-spacing" : "off",
+    'indent' : "off",
+    'allowIndentationTabs': 'false'
   }
 }
